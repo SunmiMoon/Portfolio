b1bf062dc8d36a9dfb033abf77829ba0a1a0ab1f
SunmiMoon <agaress@naver.com>
2016-09-10 17:24:56 +0900

    Scene
    작업중.

---
+++ Funghi Funghi/Assets/script/Scene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnBackButtonClicked()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnServiveButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnGameStartButtonClicked()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
