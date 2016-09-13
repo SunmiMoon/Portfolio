a0c3d23b674bc646c5ac047754f7db434b418094
SunmiMoon <agaress@naver.com>
2016-09-13 22:31:04 +0900

    Scene Change
    UI 재배치
    UI Prefab .
    Camera DontDestroyOnLoad.

---
+++ Funghi Funghi/Assets/02.Script/Scene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Scene : MonoBehaviour
+{
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnButtonClicked(int SelectIndex)
+    {
+        SceneManager.LoadScene(SelectIndex);
+    }
+}
